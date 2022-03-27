ace8f23f596fd57c130af728ef5d96066e966f54
euiraekim <harrykur139@gmail.com>
2022-03-27 20:07:42 +0900

    train_test_split.py 파일 경로 부분 수정

--- utils/train_test_split.py
+++ utils/train_test_split.py
@@ -10,25 +10,26 @@ def create_folder(save_path):
         os.makedirs(save_path)
 
 data_path = '../data'
+save_path = '../data/data_splited'
 
 files = glob(f'{data_path}/train_all/*.json')
 
 random.seed(42)
 random.shuffle(files)
 
-train_path = os.path.join(data_path, "train")
+train_path = os.path.join(save_path, "train")
 create_folder(train_path)
 
-val_path = os.path.join(data_path, "valid")
+val_path = os.path.join(save_path, "valid")
 create_folder(val_path)
 
 num_all = len(files)
 num_valid = round(0.1 * num_all)
 
 idx = 0
-for file in files:
+for file in tqdm(files):
     if idx < num_all - num_valid:
-        shutil.copyfile(file, "%s/train/%s" % (data_path, os.path.basename(file)))
+        shutil.copyfile(file, "%s/train/%s" % (save_path, os.path.basename(file)))
     else:
-        shutil.copyfile(file, "%s/valid/%s" % (data_path, os.path.basename(file)))
+        shutil.copyfile(file, "%s/valid/%s" % (save_path, os.path.basename(file)))
     idx += 1
